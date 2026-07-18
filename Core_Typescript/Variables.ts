a6e099d4620e973d8c4e4a7048c1de1832c3b0fa
learnautomation1604-cyber <learnautomation1604@gmail.com>
2026-07-18 13:15:10 +0530

    commitmessage_HeyThis is the commiut for Datatypes and Variables class _ 07182026

---
+++ Core_Typescript/Variables.ts
@@ -0,0 +1,41 @@
+// Define of Variables - Variable is a named container used to stora a data. 
+// Types of variables - var, const // ES6+ Version - let
+
+var x = 10;
+const y = 10;
+var x = 20;
+var x =100; // functional level
+let t = 20 // Let can be a block level
+
+
+console.log(x);
+console.log(y);
+console.log(t)
+
+
+
+
+class student{
+
+
+    function = async ()=>{
+        var x = 10;
+
+        function add(x: number) {
+            var y = 10;
+
+            if(x==10){
+                let z = 10;
+                console.log(y)
+
+            }
+        }
+
+
+
+     }
+
+
+
+}
+
